7ec0114e2866f30c1413d1d713a35e97c8b4c063
kojirakojira <amukas_maoto@yahoo.co.jp>
2024-01-28 12:26:57 +0900

    PLごとのCP機能追加

---
+++ pages/search/plList.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <MajorPartsH2Common>
+      {{ searchCommon().getSearchPatternName(searchPattern) }}
+    </MajorPartsH2Common>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4" lg="4" xl="4" class="col-title">
+          <v-icon>
+            mdi-pen
+          </v-icon>
+          ポケモン
+          <span class="required-mark">必須</span>
+        </v-col>
+        <v-col cols="12" md="8" lg="8" xl="8">
+          <SearchInputPokeName
+            v-model="cDtoItem.searchParams.name"
+            :keyup-enter="clickSearchBtn"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="4" lg="4" xl="4" class="col-title">
+          <v-icon>
+            mdi-pen
+          </v-icon>
+          個体値
+          <span class="required-mark">必須</span>
+        </v-col>
+        <v-col cols="12" md="8" lg="8" xl="8">
+          <SearchInputIv
+            v-model="cDtoItem.searchParams.iv"
+            :keyup-enter="clickSearchBtn"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" class="text-center">
+          <v-btn
+            rounded
+            min-width="50%"
+            color="success"
+            :disabled="isSearchBtnClick"
+            @click="clickSearchBtn"
+          >
+            検索
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <SearchResultList
+      v-if="cDtoItem.psr.goPokedexList.length !== 0"
+      :psr="cDtoItem.psr"
+      @click-row="searchCommon().clickRowResultList($event, searchPattern, cDtoItem.searchParams)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const searchPattern = 'plList'
+// current dto item
+const cDtoItem = ref<OnePokeDtoItem>({
+  searchParams: {
+    name: '',
+    iv: ''
+  },
+  psr: {
+    goPokedexList: [],
+    maybe: false
+  },
+  resData: {}
+})
+const dto: any = useAttrs().dto
+dto.params = cDtoItem
+
+const isSearchBtnClick = ref(false)
+
+// created: 画面を復元する
+searchCommon().restoreSearchScreen(['searchParams', 'psr', 'resData'], cDtoItem.value)
+
+const clickSearchBtn = async () => {
+  isSearchBtnClick.value = true
+  const msg = check()
+  if (msg) {
+    alert(msg)
+    isSearchBtnClick.value = false
+    return
+  }
+  const res: Record<string, any> = await get()
+  handleApiResult(res)
+}
+
+const check = () => {
+  let msg = ''
+  msg += validateUtils().checkRequired({ item: cDtoItem.value.searchParams.name, itemName: 'ポケモン' })
+  msg += validateUtils().checkRequired({ item: cDtoItem.value.searchParams.iv, itemName: '個体値' })
+  msg += validateUtils().checkIv({ item: cDtoItem.value.searchParams.iv, itemName: '個体値' })
+  return msg
+}
+
+const get = async () => {
+  return await fetchCommon('/api/plList', 'GET', {
+    query: {
+      name: cDtoItem.value.searchParams.name,
+      iva: cDtoItem.value.searchParams.iv.substring(0, 2),
+      ivd: cDtoItem.value.searchParams.iv.substring(2, 4),
+      ivh: cDtoItem.value.searchParams.iv.substring(4, 6)
+    }
+  })
+}
+
+/**
+   * APIのレスポンスを処理する。
+   *
+   * @param res
+   */
+const handleApiResult = (res: Record<string, any>) => {
+  const rd = res.data
+
+  // メッセージ、メッセージレベルによるハンドリング
+  const success = searchCommon().handleApiMessage(rd)
+  if (!success) {
+    isSearchBtnClick.value = false
+    return
+  }
+
+  if (rd.success) {
+    if (rd.pokemonSearchResult.unique) {
+      // 1件のみヒットした場合
+      cDtoItem.value.resData = rd
+      cDtoItem.value.psr = { goPokedexList: [], maybe: false }
+      useRouter().push({
+        name: 'search-result-plListResult',
+        query: searchCommon().makeQuery(rd.pokedexId, cDtoItem.value.searchParams)
+      })
+    } else {
+      // 複数件 or 0件ヒットした場合
+      useRouter().replace({
+        name: 'search-plList'
+      })
+      cDtoItem.value.psr = rd.pokemonSearchResult
+      isSearchBtnClick.value = false
+    }
+  }
+}
+
+useHead({
+  title: searchCommon().getSearchPatternName(searchPattern),
+  meta: [
+    { property: 'og:type', content: 'article' },
+    { property: 'og:title', content: `${searchCommon().getSearchPatternName(searchPattern)} - ペリずかん` },
+    { property: 'og:url', content: useRuntimeConfig().public.url + useRoute().path },
+    { property: 'og:site_name', content: 'ペリずかん' },
+    { property: 'og:description', content: '個体値を入力し、PLごとのCPの一覧を確認することができます。' },
+    { property: 'og:image', content: useRuntimeConfig().public.staticUrl + '/pokego/peripper-eyes.png' }
+  ]
+})
+</script>
---
+++ pages/search/result/plListResult.vue
@@ -0,0 +1,140 @@
+<template>
+  <div>
+    <MajorPartsH2Common>
+      {{ searchCommon().getSearchPatternName(searchPattern) }}
+    </MajorPartsH2Common>
+    <div v-if="!isLoading">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-card max-width="500px" class="searched-items">
+              <v-card-title class="d-block pa-2 searched-params-title">
+                検索条件
+              </v-card-title>
+              <v-card-text class="caption text-left py-1">
+                <v-container>
+                  <v-row>
+                    <v-col cols="7" md="6" lg="6" xl="6" class="pa-1">
+                      図鑑№
+                    </v-col>
+                    <v-col cols="5" md="6" lg="6" xl="6" class="pa-1">
+                      {{ editUtils().getPdxNo(cDtoItem.resData.pokedexId) }}
+                    </v-col>
+                  </v-row>
+                  <v-row class="searched-param">
+                    <v-col cols="7" md="6" lg="6" xl="6" class="pa-1">
+                      ポケモン
+                    </v-col>
+                    <v-col cols="5" md="6" lg="6" xl="6" class="pa-1">
+                      {{ editUtils().appendRemarks(cDtoItem.resData.name, cDtoItem.resData.remarks) }}
+                    </v-col>
+                  </v-row>
+                  <v-row class="searched-param" align="center">
+                    <v-col cols="7" md="6" lg="6" xl="6" class="pa-1">
+                      個体値<br>(こうげき - ぼうぎょ - HP)
+                    </v-col>
+                    <v-col cols="5" md="6" lg="6" xl="6" class="pa-1">
+                      {{ `${cDtoItem.resData.iva} - ${cDtoItem.resData.ivd} - ${cDtoItem.resData.ivh}` }}
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+      <h3>
+        PLとCP一覧
+      </h3>
+      <v-container>
+        <v-data-table
+          :headers="headers"
+          :items="cDtoItem.resData.plList"
+          items-per-page="-1"
+          class="body-2"
+        >
+          <template #[`item.percent`]="{ item }">
+            {{ item.percent + '%' }}
+          </template>
+          <template #bottom />
+        </v-data-table>
+      </v-container>
+    </div>
+    <div v-else>
+      <Loading full-page />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouteLocationNormalizedLoaded } from 'vue-router'
+const searchPattern = 'plList'
+// current dto item
+const cDtoItem = ref<ResultDtoItem>({
+  searchParams: {
+    pid: '',
+    iv: null
+  },
+  resData: {}
+})
+const dto: any = useAttrs().dto
+dto.params = cDtoItem
+
+const headers = ref<any>([
+  { title: 'PL', key: 'pl', sortable: true },
+  { title: 'CP', key: 'cp', sortable: true }
+])
+const isLoading = ref<boolean>(true)
+
+// APIアクセス用get関数
+const get = async (): Promise<Record<string, any>> => {
+  const res = await fetchCommon('/api/plList', 'GET', {
+    query: {
+      pid: cDtoItem.value.searchParams.pid,
+      iva: cDtoItem.value.searchParams.iv.substring(0, 2),
+      ivd: cDtoItem.value.searchParams.iv.substring(2, 4),
+      ivh: cDtoItem.value.searchParams.iv.substring(4, 6)
+    }
+  })
+  const rd: Record<string, any> = res.data || {}
+  if (!searchCommon().pushToast(rd?.message, rd?.msgLevel)) {
+    return {}
+  }
+  return rd
+}
+
+/**
+  * searchParams, resDataのセット
+  */
+const route: RouteLocationNormalizedLoaded = useRoute()
+cDtoItem.value.searchParams = {
+  pid: route.query.pid,
+  iv: route.query.iv
+}
+// dtoStoreからresDataを復元
+const rd: Record<string, any> | null = searchCommon().restoreResData()
+
+if (rd) {
+  cDtoItem.value.resData = rd
+} else {
+  // 存在しない場合は取得する
+  cDtoItem.value.resData = await get()
+}
+
+isLoading.value = !cDtoItem.value.resData
+
+// Head情報
+const ogpName = cDtoItem.value.resData.name
+const ogpImage = cDtoItem.value.resData.image || '/pokego/peripper-eyes.png'
+useHead({
+  title: `${ogpName}のCP`,
+  meta: [
+    { property: 'og:type', content: 'article' },
+    { property: 'og:title', content: `${ogpName}のPLごとのCP - ペリずかん` },
+    { property: 'og:url', content: useRuntimeConfig().public.url + useRoute().path },
+    { property: 'og:site_name', content: 'ペリずかん' },
+    { property: 'og:description', content: `${ogpName}のPLごとのCPを確認できます。` },
+    { property: 'og:image', content: useRuntimeConfig().public.staticUrl + ogpImage }
+  ]
+})
+</script>
