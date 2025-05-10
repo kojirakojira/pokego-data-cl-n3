bba63a1d762d828a22f787a32e516ccb4f0dae81
kojirakojira <ryo.sakuma.biz@gmail.com>
2025-05-10 20:29:00 +0900

    Redisクリーンアップ呼び出し追加

--- pages/manage/index.vue
+++ pages/manage/index.vue
@@ -17,6 +17,19 @@
             </v-btn>
           </v-col>
         </v-row>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <v-btn
+              rounded
+              min-width="50%"
+              color="success"
+              :disabled="isBtnClick"
+              @click="onClickBtn('cleanupRedis')"
+            >
+              人気な〜のRedisクリーンアップ実行
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-container>
     </div>
     <div v-show="isLoading">
@@ -27,10 +40,31 @@
 
 <script setup lang="ts">
 const isLoading = ref<boolean>(true)
+const isBtnClick = ref<boolean>(false)
 
 onMounted(async () => {
   await screenControlMethods().init()
 })
+
+const onClickBtn = async (endpoint: string) => {
+  isBtnClick.value = true
+  const userId: string = manageStore().getUserId()
+  const jwt: string = manageStore().getJwt()
+  const yn = confirm(`実行します。よろしいですか？endpoint: ${endpoint}`)
+  if (!yn) {
+    isBtnClick.value = false
+    return
+  }
+  const res = await fetchCommon(`/api/secure/${endpoint}`, 'POST', {
+    query: { userId },
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: jwt
+    }
+  })
+  alert(`実行結果：${res.data}`)
+  isBtnClick.value = false
+}
 
 const screenControlMethods = () => {
   const init = async (): Promise<void> => {
