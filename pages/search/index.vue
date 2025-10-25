f712e32173746e703b03bd83de0e9eae3246cc57
kojirakojira <ryo.sakuma.biz@gmail.com>
2025-10-25 16:52:43 +0900

    ブログ機能の削除

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -58,7 +58,6 @@
           <v-col cols="12" sm="6" md="5" lg="4" xl="4">
             <SearchTopicPage class="my-2" />
             <SearchTopicPokemon class="my-2" />
-            <BlogArchive class="my-2 pb-6" />
           </v-col>
         </v-row>
       </v-container>
